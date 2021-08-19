ef35af0106934b722aa46c5c1ce9701149dfb63e
nicat.nagizade <nicatnagizade53@gmail.com>
2021-08-19 20:31:37 +0400

    updated make script

--- scripts/make/commands/controller/index.js
+++ scripts/make/commands/controller/index.js
@@ -1,15 +1,24 @@
 const fs = require('fs')
 const path = require('path')
 const stubPath = path.join(__dirname, './controller.stub')
+const commandPath = path.join(__dirname, '../../../../', 'app/http/controllers/')
 
 const controller = (args) => {
-    const [name] = args
+    const firstParam = args[0]
+    const dirPath = firstParam.split('/')
+    const name = dirPath.pop()
     const readFile = fs.readFileSync(stubPath)
     const text = readFile.toString()
         .replace(/{Name}/g, name.capitalize())
         .replace(/{name}/g, name)
 
-    const fullPath = path.join(__dirname, '../../../../', `app/http/controllers/${name}.js`)
+    if (dirPath.length > 0) {
+        const exists = fs.existsSync(commandPath + dirPath.join('/'))
+        if(!exists) {
+            fs.mkdirSync(commandPath + dirPath.join('/'))
+        }
+    }
+    const fullPath = commandPath + firstParam + '.js'
     fs.writeFileSync(fullPath, text)
     console.log(fullPath + ' is created')
 }
--- scripts/make/commands/validator/index.js
+++ scripts/make/commands/validator/index.js
@@ -1,15 +1,24 @@
 const fs = require('fs')
 const path = require('path')
 const stubPath = path.join(__dirname, './validator.stub')
+const commandPath = path.join(__dirname, '../../../../', 'app/http/validators/')
 
 const validator = (args) => {
-    const [name] = args
+    const firstParam = args[0]
+    const dirPath = firstParam.split('/')
+    const name = dirPath.pop()
     const readFile = fs.readFileSync(stubPath)
     const text = readFile.toString()
         .replace(/{Name}/g, name.capitalize())
         .replace(/{name}/g, name)
 
-    const fullPath = path.join(__dirname, '../../../../', `app/http/validators/${name}.js`)
+    if (dirPath.length > 0) {
+        const exists = fs.existsSync(commandPath + dirPath.join('/'))
+        if(!exists) {
+            fs.mkdirSync(commandPath + dirPath.join('/'))
+        }
+    }
+    const fullPath = commandPath + firstParam + '.js'
     fs.writeFileSync(fullPath, text)
     console.log(fullPath + ' is created')
 }
