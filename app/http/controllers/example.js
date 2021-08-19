e553abe80b79d6a37dbed645a2c6bf8940d7ae5a
nicat.nagizade <nicatnagizade53@gmail.com>
2021-08-19 20:52:49 +0400

    updated

--- app/http/controllers/example.js
+++ app/http/controllers/example.js
@@ -1,33 +1,33 @@
 const BaseController = require('./')
-const ExampleService = BaseController.service('example')
-const ExampleValidator = BaseController.validator('example')
+const Service = BaseController.service('example')
+const Validator = BaseController.validator('example')
 
 class ExampleController extends BaseController {
     async index() {
         const { query } = this.req
-        const response = await ExampleService.index(query)
+        const response = await Service.index(query)
         this.success(response)
     }
     async show() {
         const { params } = this.req
-        const response = await ExampleService.show(params.id)
+        const response = await Service.show(params.id)
         this.success(response)
     }
     async store() {
         const { body } = this.req
-        const validated = ExampleValidator.store(body).validated()
-        const response = await ExampleService.store(validated)
+        const validated = Validator.store(body).validated()
+        const response = await Service.store(validated)
         this.success(response)
     }
     async update() {
         const { body, params } = this.req
-        const validated = ExampleValidator.update(body).validated()
-        const response = await ExampleService.update(params.id, validated)
+        const validated = Validator.update(body).validated()
+        const response = await Service.update(params.id, validated)
         this.success(response)
     }
     async destroy() {
         const { params } = this.req
-        const response = await ExampleService.destroy(params.id)
+        const response = await Service.destroy(params.id)
         this.success(response)
     }
 
--- routes/api/example.js
+++ routes/api/example.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const ExampleController = require('../../app/http/controllers/example')
+const Controller = require('../../app/http/controllers/example')
 
-router.get('', (...params) => { new ExampleController(...params).method('index') })
-router.post('', (...params) => { new ExampleController(...params).method('store') })
-router.get('/:id', (...params) => { new ExampleController(...params).method('show') })
-router.put('/:id', (...params) => { new ExampleController(...params).method('update') })
-router.delete('/:id', (...params) => { new ExampleController(...params).method('destroy') })
+router.get('', (...params) => { new Controller(...params).method('index') })
+router.post('', (...params) => { new Controller(...params).method('store') })
+router.get('/:id', (...params) => { new Controller(...params).method('show') })
+router.put('/:id', (...params) => { new Controller(...params).method('update') })
+router.delete('/:id', (...params) => { new Controller(...params).method('destroy') })
 
 module.exports = router
